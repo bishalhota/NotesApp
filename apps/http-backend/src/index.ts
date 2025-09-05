135efe0c8f8645c120cf419e3423dc6801448220
bishalhota <bishalhota264@gmail.com>
2025-09-05 20:37:44 +0530

    Added db package

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,30 +2,55 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware";
+import { CreateUserSchema, SignInSchema, CreateRoomSchema } from "@repo/common/types";
+
 
 const app = express();
 
-app.post("/signup",async(req,res)=>{
-
+app.post("/signup", async (req, res) => {
+    const data = CreateUserSchema.parse(req.body);
+    if (!data.success) {
+        res.json({
+            message: "Incorrect inputs"
+        })
+        return;
+    }
+    res.json({
+        userId: 12312
+    })
 })
 
-app.post("/signin", async(req,res)=>{
-    
+app.post("/signin", async (req, res) => {
+
+    const data = SignInSchema.parse(req.body);
+    if (!data.success) {
+        res.json({
+            message: "Incorrect inputs"
+        })
+        return;
+    }
+
     const userId = 1;
     const token = jwt.sign({
         userId
-    },JWT_SECRET);
+    }, JWT_SECRET);
 
     res.json({
         token
     })
 })
 
-app.post("/room",middleware,async(req,res)=>{
-
-
+app.post("/room", middleware, async (req, res) => {
+    const data = CreateRoomSchema.parse(req.body);
+    if (!data.success) {
+        res.json({
+            message: "Incorrect inputs"
+        })
+        return;
+    }
+    
     res.json({
-        roomId:12312
+        roomId: 12312
     })
 
 })
--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -3,14 +3,17 @@ import { z } from "zod";
 export const CreateUserSchema = z.object({
     username:z.string().min(3).max(20),
     password: z.string().min(6).max(20),
-    name: z.string
+    name: z.string,
+    success: z.literal(true).optional()
 })
 
 export const SignInSchema = z.object({
     username: z.string().min(3).max(20),
-    password: z.string().min(6).max(20)
+    password: z.string().min(6).max(20),
+    success: z.literal(true).optional()
 })
 
 export const CreateRoomSchema = z.object({
     roomName: z.string().min(3).max(20),
+    success: z.literal(true).optional()
 })
